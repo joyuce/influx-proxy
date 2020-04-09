970b5c2dc4271b8df85d449c83da12990d733d46
Joyuce <892989223@qq.com>
2020-04-09 16:21:29 +0800

    修改为本地依赖

--- main.go
+++ main.go
@@ -1,60 +1,60 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "github.com/chengshiwen/influx-proxy/backend"
-    "github.com/chengshiwen/influx-proxy/config"
-    "github.com/chengshiwen/influx-proxy/service"
-    "log"
-    "net/http"
-    "runtime"
-    "time"
+	"./backend"
+	"./config"
+	"./service"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"runtime"
+	"time"
 )
 
 var (
-    ConfigFile      string
-    Version         bool
-    GitCommit       string
-    BuildTime       string
+	ConfigFile string
+	Version    bool
+	GitCommit  string
+	BuildTime  string
 )
 
 func main() {
-    flag.StringVar(&ConfigFile, "config", "proxy.json", "proxy config file")
-    flag.BoolVar(&Version, "version", false, "proxy version")
-    flag.Parse()
-    if Version {
-        fmt.Printf("Version:    %s\n", config.Version)
-        fmt.Printf("Git commit: %s\n", GitCommit)
-        fmt.Printf("Go version: %s\n", runtime.Version())
-        fmt.Printf("Build time: %s\n", BuildTime)
-        fmt.Printf("OS/Arch:    %s/%s\n", runtime.GOOS, runtime.GOARCH)
-        return
-    }
+	flag.StringVar(&ConfigFile, "config", "proxy.json", "proxy config file")
+	flag.BoolVar(&Version, "version", false, "proxy version")
+	flag.Parse()
+	if Version {
+		fmt.Printf("Version:    %s\n", config.Version)
+		fmt.Printf("Git commit: %s\n", GitCommit)
+		fmt.Printf("Go version: %s\n", runtime.Version())
+		fmt.Printf("Build time: %s\n", BuildTime)
+		fmt.Printf("OS/Arch:    %s/%s\n", runtime.GOOS, runtime.GOARCH)
+		return
+	}
 
-    proxy, err := backend.NewProxy(ConfigFile)
-    if err != nil {
-        fmt.Println("config source load failed:", err.Error())
-        return
-    }
-    hs := service.HttpService{Proxy: proxy}
-    mux := http.NewServeMux()
-    hs.Register(mux)
+	proxy, err := backend.NewProxy(ConfigFile)
+	if err != nil {
+		fmt.Println("config source load failed:", err.Error())
+		return
+	}
+	hs := service.HttpService{Proxy: proxy}
+	mux := http.NewServeMux()
+	hs.Register(mux)
 
-    server := &http.Server{
-        Addr:        proxy.ListenAddr,
-        Handler:     mux,
-        IdleTimeout: config.IdleTimeout * time.Second,
-    }
-    if proxy.HTTPSEnabled {
-        log.Printf("https service start, listen on %s", server.Addr)
-        err = server.ListenAndServeTLS(proxy.HTTPSCert, proxy.HTTPSKey)
-    } else {
-        log.Printf("http service start, listen on %s", server.Addr)
-        err = server.ListenAndServe()
-    }
-    if err != nil {
-        log.Print(err)
-        return
-    }
+	server := &http.Server{
+		Addr:        proxy.ListenAddr,
+		Handler:     mux,
+		IdleTimeout: config.IdleTimeout * time.Second,
+	}
+	if proxy.HTTPSEnabled {
+		log.Printf("https service start, listen on %s", server.Addr)
+		err = server.ListenAndServeTLS(proxy.HTTPSCert, proxy.HTTPSKey)
+	} else {
+		log.Printf("http service start, listen on %s", server.Addr)
+		err = server.ListenAndServe()
+	}
+	if err != nil {
+		log.Print(err)
+		return
+	}
 }
